dfbade35a6702bbae528d1335b0058f0fb1c9efe
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-21 20:05:30 +0200

    Add play turn

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,8 @@
 class Game():
+    def __init__(self):
+        self.game_board = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
     def winner(self):
         return 0
+    
+    def takeTurn(self, player: str, position: list):
+        self.game_board[position[0]][position[1]] = player
--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -6,3 +6,8 @@ class TestGame(TestCase):
     def test_game_creation(self):
         game = Game()
         self.assertEqual(game.winner(), 0)
+    
+    def test_player_turn(self):
+        game = Game()
+        game.takeTurn("X", [0, 0])
+        self.assertEqual(game.game_board, [["X", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]])
