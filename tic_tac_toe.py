623ddfe0c73415e0f711e7aa6d2a0bb5e6fe32c4
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-22 16:28:52 +0200

    Add horizontal win

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,16 @@
-class Game():
+class Game:
     def __init__(self):
         self.game_board = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
+
     def winner(self):
+        for row in range(len(self.game_board[0])):
+            if (
+                self.game_board[row][0] == self.game_board[row][1]
+                and self.game_board[row][0] == self.game_board[row][2]
+                and self.game_board[row][0] != "0"
+            ):
+                return f"Player {self.game_board[row][0]} wins!"
         return 0
-    
+
     def takeTurn(self, player: str, position: list):
         self.game_board[position[0]][position[1]] = player
--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -2,12 +2,20 @@ from unittest import TestCase
 
 from tic_tac_toe import Game
 
+
 class TestGame(TestCase):
+    def setUp(self) -> None:
+        self.game = Game()
+
     def test_game_creation(self):
-        game = Game()
-        self.assertEqual(game.winner(), 0)
-    
+        self.assertEqual(self.game.winner(), 0)
+
     def test_player_turn(self):
-        game = Game()
-        game.takeTurn("X", [0, 0])
-        self.assertEqual(game.game_board, [["X", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]])
+        self.game.takeTurn("X", [0, 0])
+        self.assertEqual(
+            self.game_board, [["X", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
+        )
+
+    def test_horizontal_win(self):
+        self.game.game_board = [["X", "X", "X"], ["0", "0", "0"], ["0", "0", "0"]]
+        self.assertEqual(self.game.winner(), "Player X wins!")
